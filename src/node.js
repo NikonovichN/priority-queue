d4f2edcf2264f7f6f33405ebdc08d452c860dd0b
NikonovichN <nikonovichnd@gmail.com>
2019-03-18 21:23:27 +0300

    node is doesn`t comlete yet, but improve

--- src/node.js
+++ src/node.js
@@ -1,18 +1,43 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
-
+		this.parent = node;
+		/*if( this.parent == null ){
+			this.parent = node;
+		} */
+		if( this.left == null ){			
+			this.left = node;
+		}else if( this.right == null ){
+			this.right = node;
+		}
 	}
 
 	removeChild(node) {
-
+		if( this.parent == node ){
+			this.parent = node;
+		} 
+		if( this.left == node ){
+			this.left = null;
+		}else if( this.right == node ){
+			this.right = null;
+		}else{
+			throw Error(error);
+		}
 	}
 
 	remove() {
-
+		console.log( this );
+		if( this.parent != null ){
+			Node.removeChild(this.parent);
+			console.log( 'remove' );
+		}
 	}
 
 	swapWithParent() {
